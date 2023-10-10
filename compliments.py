92263a4d0c4524134ba7e366c666c4b2fe89f279
Sebastian Rusu <56532926+rususebastian02@users.noreply.github.com>
2023-10-10 10:06:06 +0100

    Create compliments.py

---
+++ compliments.py
@@ -0,0 +1,16 @@
+list = ("Gorgeous", "Beautiful", "Fantastic", "Cutie")
+print ("● List of Compliments:\n", list)
+
+x = input ("Insert a number between 0 and 3\n")
+
+if x == "0" : 
+    print("● .. is", list[0])
+
+elif x == "1" :
+    print("● .. is", list[1])
+
+elif x == "2" :
+    print("● .. is", list[2])
+
+elif x == "3" :
+    print("● .. is", list[3])
